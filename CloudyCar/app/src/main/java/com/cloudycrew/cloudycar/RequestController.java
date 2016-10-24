fb0682454339386e5e1e3131a4ef27f161aa043e
George Antonious <antoniou@ualberta.ca>
2016-10-23 18:46:41 -0600

    Use RequestController as per UML

---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/RequestController.java
@@ -0,0 +1,37 @@
+package com.cloudycrew.cloudycar;
+
+import com.cloudycrew.cloudycar.models.Route;
+import com.cloudycrew.cloudycar.models.requests.AcceptedRequest;
+import com.cloudycrew.cloudycar.models.requests.ConfirmedRequest;
+import com.cloudycrew.cloudycar.models.requests.PendingRequest;
+import com.cloudycrew.cloudycar.models.requests.Request;
+
+/**
+ * Created by George on 2016-10-23.
+ */
+
+public class RequestController {
+    public void loadRequests() {
+
+    }
+
+    public void createRequest(Route route) {
+
+    }
+
+    public void cancelRequest(Request request) {
+
+    }
+
+    public void acceptRequest(PendingRequest request) {
+
+    }
+
+    public void confirmRequest(AcceptedRequest request) {
+
+    }
+
+    public void completeRequest(ConfirmedRequest request) {
+
+    }
+}
