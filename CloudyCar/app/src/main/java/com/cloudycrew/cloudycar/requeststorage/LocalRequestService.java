c1296875e75e4e498dd06173f516122fac04305a
George Antonious <antoniou@ualberta.ca>
2016-10-24 09:18:53 -0600

    Implement LocalRequestService

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/requeststorage/LocalRequestService.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/requeststorage/LocalRequestService.java
@@ -1,19 +1,36 @@
 package com.cloudycrew.cloudycar.requeststorage;
 
+import com.cloudycrew.cloudycar.Constants;
+import com.cloudycrew.cloudycar.fileservices.IFileService;
 import com.cloudycrew.cloudycar.models.requests.AcceptedRequest;
+import com.cloudycrew.cloudycar.models.requests.CompletedRequest;
+import com.cloudycrew.cloudycar.models.requests.ConfirmedRequest;
+import com.cloudycrew.cloudycar.models.requests.PendingRequest;
 import com.cloudycrew.cloudycar.models.requests.Request;
+import com.cloudycrew.cloudycar.utils.StringUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by George on 2016-10-13.
  */
 
 public class LocalRequestService implements IRequestService {
+    IFileService fileService;
+
+    public LocalRequestService(IFileService fileService) {
+        this.fileService = fileService;
+    }
+
     @Override
     public List<Request> getRequests() {
-        return new ArrayList<>();
+        return new ArrayList<>(loadRequests().values());
     }
 
     @Override
@@ -23,17 +40,57 @@ public class LocalRequestService implements IRequestService {
 
     @Override
     public void createRequest(Request request) {
+        Map<String, Request> requests = loadRequests();
 
+        if (!requests.containsKey(request.getId())) {
+            requests.put(request.getId(), request);
+            saveRequests(requests);
+        }
     }
 
     @Override
     public void updateRequest(Request request) {
+        Map<String, Request> requests = loadRequests();
 
+        if (requests.containsKey(request.getId())) {
+            requests.put(request.getId(), request);
+            saveRequests(requests);
+        }
     }
 
     @Override
     public void deleteRequest(String requestId) {
+        Map<String, Request> requests = loadRequests();
 
+        if (requests.containsKey(requestId)) {
+            requests.remove(requestId);
+            saveRequests(requests);
+        }
     }
 
+    private Map<String, Request> loadRequests() {
+        String serializedHabits = fileService.loadFileAsString(Constants.REQUEST_MAP_FILE_NAME);
+
+        if (StringUtils.isNullOrEmpty(serializedHabits)) {
+            return new HashMap<>();
+        } else {
+            return getGson().fromJson(serializedHabits, new TypeToken<Map<String, Request>>() {}.getType());
+        }
+    }
+
+    private void saveRequests(Map<String, Request> requestMap) {
+        String serializedHabits = getGson().toJson(requestMap);
+        fileService.saveStringToFile(Constants.REQUEST_MAP_FILE_NAME, serializedHabits);
+    }
+
+    private Gson getGson() {
+        RequestDeserializer requestDeserializer = new RequestDeserializer.Builder()
+                .registerRequestType("pending", PendingRequest.class)
+                .registerRequestType("accepted", AcceptedRequest.class)
+                .registerRequestType("confirmed", ConfirmedRequest.class)
+                .registerRequestType("completed", CompletedRequest.class)
+                .build();
+
+        return new GsonBuilder().registerTypeAdapter(Request.class, requestDeserializer).create();
+    }
 }
---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.cloudycrew.cloudycar.utils;
+
+/**
+ * Created by George on 2016-10-24.
+ */
+
+public class StringUtils {
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+}
