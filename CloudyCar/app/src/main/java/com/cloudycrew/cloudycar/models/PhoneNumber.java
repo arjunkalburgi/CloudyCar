7ee8393af0f790fa158c6e788eff00b62eb8ead8
Ryan Huard <ryan.huard@live.ca>
2016-10-29 17:48:42 -0600

    Phone number model is implemented. Upon being given a phone number it will parse out the numbers in it into a validated number and then set this

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/models/PhoneNumber.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/models/PhoneNumber.java
@@ -5,11 +5,47 @@ package com.cloudycrew.cloudycar.models;
  */
 
 public class PhoneNumber {
+
+    private String phoneNumber;
     public PhoneNumber(String number) {
 
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws InvalidPhoneNumberException
+    {
+        if (!validate(phoneNumber))
+        {
+            throw new InvalidPhoneNumberException();
+        }
+    }
+
+
+    private boolean validate(String phoneNumber)
+    {
+        String validatedNumber = "";
+        for (char c: phoneNumber.toCharArray())
+        {
+            if(Character.isDigit(c))
+            {
+                validatedNumber += c;
+            }
+        }
+        //We only support normal phone numbers or something....
+        if (validatedNumber.length() < 10)
+        {
+            return false;
+        }
+        this.phoneNumber = validatedNumber;
+        return true;
     }
 
     public static class InvalidPhoneNumberException extends RuntimeException {
 
     }
+
 }
