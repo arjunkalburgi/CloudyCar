552719376a8eb351d63a9c77b8a59ff6c3e83dee
Arjun Kalburgi <=>
2016-10-30 19:33:17 -0600

    Sign in screen complete with activity (consider changing activity name)

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/MainActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button lessgo = (Button) findViewById(R.id.startButton);
+        lessgo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
