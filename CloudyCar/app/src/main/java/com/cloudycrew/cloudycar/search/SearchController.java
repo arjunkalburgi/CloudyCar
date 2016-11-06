e6b5788aced72045ff68c1aa12e9e1de12765f45
George Antonious <antoniou@ualberta.ca>
2016-11-05 20:15:21 -0600

    Stub out search

---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/search/SearchController.java
@@ -0,0 +1,18 @@
+package com.cloudycrew.cloudycar.search;
+
+import com.cloudycrew.cloudycar.ViewController;
+import com.cloudycrew.cloudycar.models.Point;
+
+/**
+ * Created by George on 2016-11-05.
+ */
+
+public class SearchController extends ViewController<ISearchView> {
+    public void searchByPoint(Point point) {
+
+    }
+
+    public void searchByKeyword(String keyword) {
+
+    }
+}
