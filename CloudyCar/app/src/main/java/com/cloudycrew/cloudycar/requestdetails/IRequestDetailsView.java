5d95a2c1ae8b138edb5a17299402dac42b46244f
George Antonious <antoniou@ualberta.ca>
2016-11-05 20:06:57 -0600

    Stub request details

---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/requestdetails/IRequestDetailsView.java
@@ -0,0 +1,11 @@
+package com.cloudycrew.cloudycar.requestdetails;
+
+import com.cloudycrew.cloudycar.models.requests.Request;
+
+/**
+ * Created by George on 2016-11-05.
+ */
+
+public interface IRequestDetailsView {
+    void displayRequest(Request request);
+}
