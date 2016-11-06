6a3ddd1b11eccac5607693c38d08b0d0dbc75388
George Antonious <antoniou@ualberta.ca>
2016-11-05 19:54:36 -0600

    Stub out DriverSummary

---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/ViewController.java
@@ -0,0 +1,17 @@
+package com.cloudycrew.cloudycar;
+
+/**
+ * Created by George on 2016-11-05.
+ */
+
+public class ViewController<T> {
+    private T view;
+
+    public void attachView(T view) {
+        this.view = view;
+    }
+
+    public void detachView() {
+        this.view = null;
+    }
+}
---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/driversummary/DriverSummaryFragment.java
@@ -0,0 +1,28 @@
+package com.cloudycrew.cloudycar.driversummary;
+
+import com.cloudycrew.cloudycar.BaseFragment;
+import com.cloudycrew.cloudycar.models.requests.AcceptedRequest;
+import com.cloudycrew.cloudycar.models.requests.ConfirmedRequest;
+
+import java.util.List;
+
+/**
+ * Created by George on 2016-11-05.
+ */
+
+public class DriverSummaryFragment extends BaseFragment implements IDriverSummaryView {
+    @Override
+    public void displayLoading() {
+
+    }
+
+    @Override
+    public void displayAcceptedRequests(List<AcceptedRequest> acceptedRequests) {
+
+    }
+
+    @Override
+    public void displayConfirmedRequests(List<ConfirmedRequest> confirmedRequests) {
+
+    }
+}
