44ef5fd09ffed6d370bc576fce4ec2b2442661f5
George Antonious <antoniou@ualberta.ca>
2016-11-09 23:27:12 -0700

    Interact with SignUpController in SignUpActivity

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/signup/SignUpActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/signup/SignUpActivity.java
@@ -2,25 +2,26 @@ package com.cloudycrew.cloudycar.signup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.cloudycrew.cloudycar.BaseActivity;
-import com.cloudycrew.cloudycar.ISignUpView;
 import com.cloudycrew.cloudycar.R;
 import com.cloudycrew.cloudycar.SignUpCompleteActivity;
-import com.cloudycrew.cloudycar.controllers.UserController;
-import com.cloudycrew.cloudycar.email.Email;
-import com.cloudycrew.cloudycar.models.PhoneNumber;
 import com.cloudycrew.cloudycar.models.User;
 
-public class SignUpActivity extends BaseActivity implements ISignUpView {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    String username;
-    Email email;
-    PhoneNumber phoneNumber;
+public class SignUpActivity extends BaseActivity implements ISignUpView {
+    @BindView(R.id.signup_username)
+    protected EditText usernameEditText;
+    @BindView(R.id.signup_email)
+    protected EditText emailEditText;
+    @BindView(R.id.signup_phone)
+    protected EditText phoneEditText;
+
+    private SignUpController signUpController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +29,49 @@ public class SignUpActivity extends BaseActivity implements ISignUpView {
         setContentView(R.layout.activity_sign_up);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
-
-        EditText usernamestr = (EditText) this.findViewById(R.id.signup_username);
-        username = usernamestr.getText().toString();
-        EditText emailstr = (EditText) this.findViewById(R.id.signup_email);
-        email = new Email(emailstr.getText().toString());
-        EditText phonestr = (EditText) this.findViewById(R.id.signup_phone);
-        phoneNumber = new PhoneNumber(phonestr.getText().toString());
-
-        Button lessgo = (Button) findViewById(R.id.startButton);
-        lessgo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signUp();
-            }
-        });
+        ButterKnife.bind(this);
+        resolveDependencies();
     }
 
-    public void signUp() {
-        User u = new User(username);
-        u.setEmail(email);
-        u.setPhoneNumber(phoneNumber);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        signUpController.attachView(this);
+    }
 
-        //TODO: Make user!
+    @Override
+    protected void onPause() {
+        super.onPause();
+        signUpController.detachView();
+    }
 
+    private void resolveDependencies() {
+        signUpController = getCloudyCarApplication().getSignUpController();
+    }
+
+
+    @OnClick(R.id.startButton)
+    protected void onStartButtonClicked() {
+        signUpController.registerUser(usernameEditText.getText().toString(),
+                                      emailEditText.getText().toString(),
+                                      phoneEditText.getText().toString());
+    }
+
+    @Override
+    public void onMalformedUserFailure() {
+
+    }
+
+
+    @Override
+    public void onDuplicateUsernameFailure() {
+
+    }
+
+    @Override
+    public void onSuccessfulRegistration() {
         Intent intent = new Intent(this, SignUpCompleteActivity.class);
         startActivity(intent);
+        finish();
     }
 }
