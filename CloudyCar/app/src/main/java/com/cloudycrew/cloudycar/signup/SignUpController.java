a78aaa7c4dc1b9c88a5c0c4a19948bc33e0d711c
George Antonious <antoniou@ualberta.ca>
2016-11-09 22:55:52 -0700

    Handle attempting to create a user with a bad email or phone number

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/signup/SignUpController.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/signup/SignUpController.java
@@ -2,6 +2,8 @@ package com.cloudycrew.cloudycar.signup;
 
 import com.cloudycrew.cloudycar.ViewController;
 import com.cloudycrew.cloudycar.controllers.UserController;
+import com.cloudycrew.cloudycar.email.Email;
+import com.cloudycrew.cloudycar.models.PhoneNumber;
 import com.cloudycrew.cloudycar.models.User;
 import com.cloudycrew.cloudycar.scheduling.ISchedulerProvider;
 import com.cloudycrew.cloudycar.utils.ObservableUtils;
@@ -19,12 +21,37 @@ public class SignUpController extends ViewController<ISignUpView> {
         this.schedulerProvider = schedulerProvider;
     }
 
-    public void registerUser(User user) {
+    public void registerUser(String username, String email, String phoneNumber) {
+        try {
+            User newUser = createUser(username, email, phoneNumber);
+            registerUser(newUser);
+        } catch (Exception e) {
+            dispatchOnMalformedUserFailure();
+        }
+    }
+
+    private void registerUser(User user) {
         ObservableUtils.fromAction(userController::createUser, user)
                        .subscribeOn(schedulerProvider.ioScheduler())
                        .observeOn(schedulerProvider.mainThreadScheduler())
                        .subscribe(nothing -> dispatchOnSuccessfulRegistration(),
                                   throwable -> dispatchOnDuplicateUsernameFailure());
+
+    }
+
+    private User createUser(String username, String email, String phoneNumber) {
+        User newUser = new User(username);
+        newUser.setEmail(new Email(email));
+        newUser.setPhoneNumber(new PhoneNumber(email));
+
+        return newUser;
+    }
+
+
+    private void dispatchOnMalformedUserFailure() {
+        if (isViewAttached()) {
+            getView().onMalformedUserFailure();
+        }
     }
 
     private void dispatchOnDuplicateUsernameFailure() {
