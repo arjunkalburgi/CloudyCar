7f615021714aa247a8681f268d8ced00c8678893
Arjun Kalburgi <askalburgi@gmail.com>
2016-11-09 20:34:31 -0700

    Sign up things. For George

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/SignUpActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/SignUpActivity.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity implements ISignUpView {
+import com.cloudycrew.cloudycar.controllers.UserController;
+import com.cloudycrew.cloudycar.email.Email;
+import com.cloudycrew.cloudycar.models.PhoneNumber;
+import com.cloudycrew.cloudycar.models.User;
+
+public class SignUpActivity extends BaseActivity implements ISignUpView {
+
+    String username;
+    Email email;
+    PhoneNumber phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,13 @@ public class SignUpActivity extends AppCompatActivity implements ISignUpView {
         setContentView(R.layout.activity_sign_up);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        EditText usernamestr = (EditText) this.findViewById(R.id.signup_username);
+        username = usernamestr.getText().toString();
+        EditText emailstr = (EditText) this.findViewById(R.id.signup_email);
+        email = new Email(emailstr.getText().toString());
+        EditText phonestr = (EditText) this.findViewById(R.id.signup_phone);
+        phoneNumber = new PhoneNumber(phonestr.getText().toString());
 
         Button lessgo = (Button) findViewById(R.id.startButton);
         lessgo.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +42,12 @@ public class SignUpActivity extends AppCompatActivity implements ISignUpView {
     }
 
     public void signUp() {
+        User u = new User(username);
+        u.setEmail(email);
+        u.setPhoneNumber(phoneNumber);
+
+        //TODO: Make user!
+
         Intent intent = new Intent(this, SignUpCompleteActivity.class);
         startActivity(intent);
     }
