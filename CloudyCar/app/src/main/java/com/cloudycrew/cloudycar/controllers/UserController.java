18f9d8050fc040034f46900ba262b5e33a74c272
Ryan Huard <ryan.huard@live.ca>
2016-11-09 17:40:50 -0700

    UserController stuff.. seems kind of redundant!

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/controllers/UserController.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.cloudycrew.cloudycar.controllers;
 
 import com.cloudycrew.cloudycar.models.User;
+import com.cloudycrew.cloudycar.users.DuplicateUserException;
 import com.cloudycrew.cloudycar.users.IUserService;
+import com.cloudycrew.cloudycar.users.IncompleteUserException;
 
 /**
  * Created by George on 2016-10-23.
@@ -12,13 +14,21 @@ public class UserController {
     private IUserService userService;
 
     public UserController(IUserService userService) {
-
+        this.userService = userService;
     }
-    public void createUser() {
-
+    public void createUser(User user) throws DuplicateUserException, IncompleteUserException {
+        userService.createUser(user);
     }
 
     public User getUser(String username) {
-        return null;
+        return userService.getUser(username);
+    }
+
+    public User getCurrentUser() {
+        return userService.getCurrentUser();
+    }
+
+    public void updateUser(User user) {
+        userService.updateUser(user);
     }
 }
