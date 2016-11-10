966bbadcbef6c984b867fa5f0f1079316fed3423
Ryan Huard <ryan.huard@live.ca>
2016-11-09 21:05:15 -0700

    Pushing for my desktop

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/userprofile/UserProfileActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/userprofile/UserProfileActivity.java
@@ -1,10 +1,16 @@
 package com.cloudycrew.cloudycar.userprofile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cloudycrew.cloudycar.BaseActivity;
 import com.cloudycrew.cloudycar.R;
+import com.cloudycrew.cloudycar.controllers.UserController;
+import com.cloudycrew.cloudycar.elasticsearch.ElasticSearchService;
 import com.cloudycrew.cloudycar.models.User;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by George on 2016-11-05.
@@ -18,7 +24,12 @@ public class UserProfileActivity extends BaseActivity implements IUserProfileVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_details_summary);
 
+        Intent myIntent = getIntent();
+        String username = myIntent.getStringExtra("username");
+
         resolveDependencies();
+
+        userProfileController.loadUser(username);
     }
 
     @Override
@@ -43,6 +54,14 @@ public class UserProfileActivity extends BaseActivity implements IUserProfileVie
 
     @Override
     public void displayUser(User user) {
+        TextView usernameView = (TextView)findViewById(R.id.username);
+        TextView phoneNumberView = (TextView)findViewById(R.id.PhoneNumberText);
+        TextView emailAddressView = (TextView)findViewById(R.id.emailAddressText);
 
+        usernameView.setText(user.getUsername());
+        phoneNumberView.setText(user.getPhoneNumber().getPhoneNumber());
+        emailAddressView.setText(user.getEmail().getEmail());
+
+        //Probably set the image here
     }
 }
