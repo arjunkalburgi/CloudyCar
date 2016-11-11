046b96bbc74af9ebfe3139b3c9fbb06413718061
Loonman <ryan.huard@live.ca>
2016-11-11 15:04:09 -0700

    Skeleton class? Made out of bones shaped like a class... he's got skeleton attributes, skeleton methods! OOP strength at his command, it's skeleton class!

---
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/userprofile/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package com.cloudycrew.cloudycar.userprofile;
+
+import android.os.Bundle;
+
+import com.cloudycrew.cloudycar.BaseActivity;
+import com.cloudycrew.cloudycar.R;
+
+public class EditProfileActivity extends BaseActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
