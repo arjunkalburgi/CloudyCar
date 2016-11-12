e4e855a48176a5822c9cfe586471b016a5f00ad1
George Antonious <antoniou@ualberta.ca>
2016-11-12 11:31:02 -0700

    Added SearchActivity

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/search/SearchActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/search/SearchActivity.java
@@ -1,26 +1,47 @@
 package com.cloudycrew.cloudycar.search;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.cloudycrew.cloudycar.BaseActivity;
+import com.cloudycrew.cloudycar.R;
+import com.cloudycrew.cloudycar.RequestAdapter;
 import com.cloudycrew.cloudycar.models.requests.PendingRequest;
 import com.cloudycrew.cloudycar.models.requests.Request;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by George on 2016-11-05.
  */
 
 public class SearchActivity extends BaseActivity implements ISearchView {
+    @BindView(R.id.toolbar)
+    protected Toolbar toolbar;
+    @BindView(R.id.search_loading_progress_bar)
+    protected ProgressBar searchProgressBar;
+    @BindView(R.id.search_results_recycler_view)
+    protected RecyclerView searchRecyclerView;
+
+    private RequestAdapter requestAdapter;
     private SearchController searchController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO: setContentView
+        setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
         resolveDependencies();
+        setUpRecyclerView();
     }
 
     @Override
@@ -38,13 +59,20 @@ public class SearchActivity extends BaseActivity implements ISearchView {
         searchController = getCloudyCarApplication().getSearchController();
     }
 
+    private void setUpRecyclerView() {
+        requestAdapter = new RequestAdapter();
+        searchRecyclerView.setAdapter(requestAdapter);
+        searchRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    }
+
     @Override
     public void showLoading() {
-        
+        searchProgressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void showSearchResults(List<PendingRequest> searchResults) {
-
+        searchProgressBar.setVisibility(View.GONE);
+        requestAdapter.setPendingRequests(searchResults);
     }
 }
