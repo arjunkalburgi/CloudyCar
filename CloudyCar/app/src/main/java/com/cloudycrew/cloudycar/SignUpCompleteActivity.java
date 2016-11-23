c4e2a465844bc604329bf2674400727c5724fc13
Arjun Kalburgi <askalburgi@gmail.com>
2016-11-22 17:56:15 -0700

    Implemented Alert Dialog

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/SignUpCompleteActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/SignUpCompleteActivity.java
@@ -1,12 +1,19 @@
 package com.cloudycrew.cloudycar;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpCompleteActivity extends AppCompatActivity {
+public class SignUpCompleteActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,40 @@ public class SignUpCompleteActivity extends AppCompatActivity {
     private void startDriverSummary() {
         Intent intent = new Intent(this, SummaryActivity.class);
         intent.putExtra("mode", "driver");
-        startActivity(intent);
+        startAddVehicleDescription();
+//        startActivity(intent);
     }
+
+    private void startAddVehicleDescription() {
+        final View inputView = LayoutInflater.from(this).inflate(R.layout.car_info_input_dialog, null);
+        Activity a = this;
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Set Car information")
+                .setView(inputView)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Log.d("SignUpCompleteActivity", "Thing");
+                        EditText carDescription = (EditText) inputView.findViewById(R.id.CarInfo);
+                        if (carDescription.getText().toString().trim() == "") {return;}
+
+                        // TODO: add car description to user.
+
+                        Intent intent = new Intent(a, SummaryActivity.class);
+                        intent.putExtra("mode", "driver");
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "You cannot be a driver without car information", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
 }
