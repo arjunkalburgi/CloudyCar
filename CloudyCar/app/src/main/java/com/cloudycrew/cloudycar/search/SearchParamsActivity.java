7f3ebee181956687aa5268e6743f6dae50b19715
Steven Boddez <stevenboddez@gmail.com>
2016-11-27 16:03:48 -0700

    Price field hint changes with radio button change

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/search/SearchParamsActivity.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/search/SearchParamsActivity.java
@@ -2,17 +2,18 @@ package com.cloudycrew.cloudycar.search;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.cloudycrew.cloudycar.R;
 import com.cloudycrew.cloudycar.models.Location;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +24,10 @@ public class SearchParamsActivity extends AppCompatActivity {
     protected Button submitButton;
     @BindView(R.id.search_radius_spinner)
     protected Spinner radiusSpinner;
+    @BindView(R.id.search_price_wrapper)
+    protected TextInputLayout searchPrice;
+    @BindView(R.id.search_filter_group)
+    protected RadioGroup searchFilterGroup;
 
     private Location userSelectedLocation;
     private int[] radiusValues;
@@ -40,6 +45,14 @@ public class SearchParamsActivity extends AppCompatActivity {
                 R.array.search_radius_choices, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         radiusSpinner.setAdapter(spinnerAdapter);
+
+        searchFilterGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup rg, int checked) {
+                RadioButton radioButton = (RadioButton) findViewById(checked);
+                searchPrice.setHint(radioButton.getText().toString());
+            }
+        });
     }
 
     @OnClick(R.id.search_choose_location)
