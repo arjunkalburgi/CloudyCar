370f5909c6e4d09254a527980ed5955af2659327
Steven Boddez <stevenboddez@gmail.com>
2016-11-27 22:18:50 -0700

    Removed janky color use when requests get moved in the list; made snackbar more readable

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/ridersummary/RiderSummaryFragment.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/ridersummary/RiderSummaryFragment.java
@@ -20,6 +20,7 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.cloudycrew.cloudycar.BaseFragment;
 import com.cloudycrew.cloudycar.Constants;
@@ -77,7 +78,6 @@ public class RiderSummaryFragment extends BaseFragment implements IRiderSummaryV
         setUpSwipeRefreshLayout();
 
         setUpItemTouchHelper(view);
-        setUpAnimationDecoratorHelper();
 
         return view;
     }
@@ -256,7 +256,7 @@ public class RiderSummaryFragment extends BaseFragment implements IRiderSummaryV
             int xMarkMargin;
             boolean initiated;
             Snackbar snackbar = Snackbar
-                    .make(v, "Request Canceled", Snackbar.LENGTH_LONG)
+                    .make(v, "Request cancelled", Snackbar.LENGTH_LONG)
                     .setAction("Undo", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -273,6 +273,7 @@ public class RiderSummaryFragment extends BaseFragment implements IRiderSummaryV
                             pendingRemovals.clear();
                         }
                     });
+
             ArrayList<ViewCell> pendingRemovals = new ArrayList<ViewCell>();
 
             private void init() {
@@ -281,6 +282,8 @@ public class RiderSummaryFragment extends BaseFragment implements IRiderSummaryV
                 xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
                 xMarkMargin = (int) getActivity().getResources().getDimension(R.dimen.ic_clear_margin);
                 initiated = true;
+
+                snackbar.setActionTextColor(Color.WHITE);
             }
 
             // not important, we don't want drag & drop
@@ -348,87 +351,6 @@ public class RiderSummaryFragment extends BaseFragment implements IRiderSummaryV
         };
         ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         mItemTouchHelper.attachToRecyclerView(requestView);
-    }
-
-    // ItemDecorator for drawing the red background
-    private void setUpAnimationDecoratorHelper() {
-        requestView.addItemDecoration(new RecyclerView.ItemDecoration() {
-
-            // we want to cache this and not allocate anything repeatedly in the onDraw method
-            Drawable background;
-            boolean initiated;
-
-            private void init() {
-                background = new ColorDrawable(getResources().getColor(R.color.deleteRed));
-                initiated = true;
-            }
-
-            @Override
-            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-
-                if (!initiated) {
-                    init();
-                }
-
-                // only if animation is in progress
-                if (parent.getItemAnimator().isRunning()) {
-
-                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
-                    // this is not exclusive, both movement can be happening at the same time
-                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
-                    // then remove one from the middle
-
-                    // find first child with translationY > 0
-                    // and last one with translationY < 0
-                    // we're after a rect that is not covered in recycler-view views at this point in time
-                    View lastViewComingDown = null;
-                    View firstViewComingUp = null;
-
-                    // this is fixed
-                    int left = 0;
-                    int right = parent.getWidth();
-
-                    // this we need to find out
-                    int top = 0;
-                    int bottom = 0;
-
-                    // find relevant translating views
-                    int childCount = parent.getLayoutManager().getChildCount();
-                    for (int i = 0; i < childCount; i++) {
-                        View child = parent.getLayoutManager().getChildAt(i);
-                        if (child.getTranslationY() < 0) {
-                            // view is coming down
-                            lastViewComingDown = child;
-                        } else if (child.getTranslationY() > 0) {
-                            // view is coming up
-                            if (firstViewComingUp == null) {
-                                firstViewComingUp = child;
-                            }
-                        }
-                    }
-
-                    if (lastViewComingDown != null && firstViewComingUp != null) {
-                        // views are coming down AND going up to fill the void
-                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
-                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
-                    } else if (lastViewComingDown != null) {
-                        // views are going down to fill the void
-                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
-                        bottom = lastViewComingDown.getBottom();
-                    } else if (firstViewComingUp != null) {
-                        // views are coming up to fill the void
-                        top = firstViewComingUp.getTop();
-                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
-                    }
-
-                    background.setBounds(left, top, right, bottom);
-                    background.draw(c);
-
-                }
-                super.onDraw(c, parent, state);
-            }
-
-        });
     }
 
 }
