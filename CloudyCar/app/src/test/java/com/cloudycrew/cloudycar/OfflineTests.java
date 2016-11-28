4335ac4f7512eede487e83c02f2bbef35ba370fc
Ryan Huard <ryan.huard@live.ca>
2016-11-27 22:35:12 -0700

    Offline tests are now properly written

--- CloudyCar/app/src/test/java/com/cloudycrew/cloudycar/OfflineTests.java
+++ CloudyCar/app/src/test/java/com/cloudycrew/cloudycar/OfflineTests.java
@@ -6,6 +6,8 @@ import com.cloudycrew.cloudycar.elasticsearch.ElasticSearchConnectivityException
 import com.cloudycrew.cloudycar.models.Location;
 import com.cloudycrew.cloudycar.models.Route;
 import com.cloudycrew.cloudycar.models.User;
+import com.cloudycrew.cloudycar.models.requests.CancelledRequest;
+import com.cloudycrew.cloudycar.models.requests.ConfirmedRequest;
 import com.cloudycrew.cloudycar.models.requests.PendingRequest;
 import com.cloudycrew.cloudycar.models.requests.Request;
 import com.cloudycrew.cloudycar.requeststorage.CloudRequestService;
@@ -22,6 +24,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -59,7 +62,12 @@ public class OfflineTests {
     private PendingRequest acceptedRequest1;
     private PendingRequest newAcceptedRequest;
 
+    private CancelledRequest newCancelledRequest;
+    private ConfirmedRequest newConfirmedRequest;
+
     private String requestDescription;
+
+    private ArrayList<Request> cloudRequests;
 
     @Before
     public void set_up() {
@@ -88,8 +96,15 @@ public class OfflineTests {
 
         newRequest = new PendingRequest(rider.getUsername(), route, price, requestDescription);
 
+        newCancelledRequest = newRequest.cancel();
+
+
+
         acceptedRequest1 = request1.accept(driver.getUsername());
         newAcceptedRequest = newRequest.accept(driver.getUsername());
+        newConfirmedRequest = newAcceptedRequest.confirmRequest(driver.getUsername());
+        cloudRequests = new ArrayList<>();
+        cloudRequests.add(newAcceptedRequest);
     }
 
     @Test
@@ -117,46 +132,59 @@ public class OfflineTests {
         doNothing().when(cloudRequestService).createRequest(any(Request.class));
         connectivityService.setInternetConnectivity(true);
 
-        verify(cloudRequestService).createRequest(newRequest);
+        verify(requestQueue).dequeueCreation(newRequest);
+        verify(cloudRequestService, times(2)).createRequest(newRequest);
     }
     
     @Test
     public void test_acceptRequest_ifTheDeviceIsOffline_thenSendsAcceptedRequestWhenDeviceRegainsConnectivity() {
         when(geoDecoder.decodeLatLng(anyDouble(), anyDouble())).thenReturn("Test address");
+        doThrow(new ElasticSearchConnectivityException(new IOException())).when(cloudRequestService).updateRequest(any(Request.class));
+        when(requestQueue.getAcceptedQueue()).thenReturn(Arrays.<PendingRequest>asList(newAcceptedRequest));
+        when(cloudRequestService.getRequests()).thenReturn(new ArrayList<Request>(cloudRequests));
         connectivityService.setInternetConnectivity(false);
 
-        compositeRequestService.createRequest(newAcceptedRequest);
-        verify(cloudRequestService, never()).updateRequest(newAcceptedRequest);
+        compositeRequestService.updateRequest(newAcceptedRequest);
+        verify(requestQueue).enqueueAccept(newAcceptedRequest);
 
-
+        doNothing().when(cloudRequestService).updateRequest(any(Request.class));
         connectivityService.setInternetConnectivity(true);
 
-        verify(cloudRequestService).updateRequest(newAcceptedRequest);
+        verify(requestQueue).dequeueAccept(newAcceptedRequest);
+        verify(cloudRequestService, times(2)).updateRequest(any(Request.class));
     }
 
     @Test
-    public void test_cancelRequest_ifTheDeviceIsOffline_thenSendsCancelledRequestWhenDeviceRegainsConnectivity() {
+    public void test_cancelRequest_ifTheDeviceIsOffline_thenSendsRequestWhenDeviceRegainsConnectivity() {
         when(geoDecoder.decodeLatLng(anyDouble(), anyDouble())).thenReturn("Test address");
+        doThrow(new ElasticSearchConnectivityException(new IOException())).when(cloudRequestService).updateRequest(any(Request.class));
+        when(requestQueue.getCancellationQueue()).thenReturn(Arrays.<CancelledRequest>asList(newCancelledRequest));
         connectivityService.setInternetConnectivity(false);
 
-        compositeRequestService.createRequest(newAcceptedRequest);
-        verify(cloudRequestService, never()).updateRequest(newAcceptedRequest);
+        compositeRequestService.updateRequest(newCancelledRequest);
+        verify(requestQueue).enqueueCancellation(newCancelledRequest);
 
+        doNothing().when(cloudRequestService).updateRequest(any(Request.class));
         connectivityService.setInternetConnectivity(true);
 
-        verify(cloudRequestService).updateRequest(newAcceptedRequest);
+        verify(requestQueue).dequeueCancellation(newCancelledRequest);
+        verify(cloudRequestService, times(2)).updateRequest(newCancelledRequest);
     }
 
     @Test
     public void test_confirmRequest_ifTheDeviceIsOffline_thenSendsConfirmedRequestWhenDeviceRegainsConnectivity() {
         when(geoDecoder.decodeLatLng(anyDouble(), anyDouble())).thenReturn("Test address");
+        doThrow(new ElasticSearchConnectivityException(new IOException())).when(cloudRequestService).updateRequest(any(Request.class));
+        when(requestQueue.getConfirmationQueue()).thenReturn(Arrays.<ConfirmedRequest>asList(newConfirmedRequest));
         connectivityService.setInternetConnectivity(false);
 
-        compositeRequestService.createRequest(newAcceptedRequest);
-        verify(cloudRequestService, never()).updateRequest(newAcceptedRequest);
+        compositeRequestService.updateRequest(newConfirmedRequest);
+        verify(requestQueue).enqueueConfirmation(newConfirmedRequest);
 
+        doNothing().when(cloudRequestService).updateRequest(any(Request.class));
         connectivityService.setInternetConnectivity(true);
 
-        verify(cloudRequestService).updateRequest(newAcceptedRequest);
+        verify(requestQueue).dequeueConfirmation(newConfirmedRequest);
+        verify(cloudRequestService, times(2)).updateRequest(newConfirmedRequest);
     }
 }
