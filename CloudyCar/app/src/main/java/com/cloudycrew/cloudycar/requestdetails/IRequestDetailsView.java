91a59566f577c2a447fab278941b9db32de3240b
George Antonious <antoniou@ualberta.ca>
2016-11-28 00:12:59 -0700

    Added missing javadocs

--- CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/requestdetails/IRequestDetailsView.java
+++ CloudyCar/app/src/main/java/com/cloudycrew/cloudycar/requestdetails/IRequestDetailsView.java
@@ -7,9 +7,25 @@ import com.cloudycrew.cloudycar.models.requests.PendingRequest;
 /**
  * Created by George on 2016-11-05.
  */
-
 public interface IRequestDetailsView {
+    /**
+     * Callback to display pending request.
+     *
+     * @param pendingRequest the pending request
+     */
     void displayPendingRequest(PendingRequest pendingRequest);
+
+    /**
+     * Callback to display confirmed request.
+     *
+     * @param confirmedRequest the confirmed request
+     */
     void displayConfirmedRequest(ConfirmedRequest confirmedRequest);
+
+    /**
+     * Callback to display completed request.
+     *
+     * @param completedRequest the completed request
+     */
     void displayCompletedRequest(CompletedRequest completedRequest);
 }
